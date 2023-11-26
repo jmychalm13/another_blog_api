2b533d9514c96615a94e86b6808974129b33b423
“Jessica <jmychalm13@gmail.com>
2023-11-26 09:20:04 -0500

    added bcrypt gem and user authentication and user controller, passing test

---
+++ db/schema.rb
@@ -0,0 +1,30 @@
+
+
+ActiveRecord::Schema[7.0].define(version: 2023_11_26_004427) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
